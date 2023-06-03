036428811f9bf149d13f8e203648422f80d0e17b
Your Name <you@example.com>
2023-06-03 05:37:17 +0530

    Library msnagement design

---
+++ LibraryManagementSystem/src/main/java/com/management/Librarian.java
@@ -0,0 +1,16 @@
+package com.management;
+
+class Librarian extends Account {
+    Librarian(String id, String pass, User person) {
+        super(id, pass, person);
+    }
+
+    public void addBook(Book book, Catalog bookDatabase) {
+        bookDatabase.updateBook(book);
+        System.out.println("Done");
+    }
+
+    public void registerUser() {
+
+    }
+}
---
+++ LibraryManagementSystem/src/main/java/com/management/Library.java
@@ -0,0 +1,20 @@
+package com.management;
+
+class Library {
+    private String name, address;
+
+    Library(String name, String address) {
+
+        this.name = name;
+        this.address = address;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+}
